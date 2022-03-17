8c6d489cb7e56115e1af85f8f664392564761b79
Ana Flavia Lopes <anaflavialopes13@hotmail.com>
2022-03-17 11:17:16 -0300

    Primeira Versão

---
+++ Contato.java
@@ -0,0 +1,114 @@
+package com.senac.agendatelefonica;
+
+public class Contato {
+    //Atributos
+    private int código;
+    private String nome;
+    private String endereço;
+    private String cidade;
+    private String estado;
+    private String telefone;
+    private String celular;
+    private String email;
+    private String dataNasc;
+    private String comentário;
+    
+    //Métodos
+    public void incluir(){
+        
+    }
+    
+    public void salvar(){
+        
+    }
+    
+    public void excluir(){
+        
+    }
+    
+    public void sair (){
+        
+    }
+
+    public void setCódigo(int código) {
+        this.código = código;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public void setComentário(String comentário) {
+        this.comentário = comentário;
+    }
+
+    public int getCódigo() {
+        return código;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public String getComentário() {
+        return comentário;
+    }
+    
+    
+}
